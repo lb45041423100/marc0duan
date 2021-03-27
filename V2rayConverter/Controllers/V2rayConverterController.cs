9eb7785b38e564a9510403bfad5a765428da41db
lb45041423100 <obukhovskiy85@list.ru>
2021-03-27 15:41:48 +0800

    initial project

---
+++ V2rayConverter/Controllers/V2rayConverterController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SubscribeManager;
+namespace V2rayConverter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class V2rayConverterController : ControllerBase
+    {
+        
+
+        private readonly ILogger<V2rayConverterController> _logger;
+
+        public V2rayConverterController(ILogger<V2rayConverterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            try
+            {
+                var url = Request.Query["url"];
+                var convertUrl = new ConvertUrl(url);
+                return convertUrl.result;
+            }
+            catch
+            {
+                throw new Exception("arg error");
+            }
+            
+            
+        }
+    }
+}
